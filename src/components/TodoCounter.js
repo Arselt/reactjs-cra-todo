70b0a2bb5dac9ec1a9476fc36eb5850d6d995be5
Arath Astorga <arastorga@outlook.com>
2021-10-17 21:37:37 -0500

    ✨ React useContext and Folder restructure

--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { TodoContext } from '../TodoContext';
 import './TodoCounter.css';
 
-function TodoCounter({total, completed}) {
+function TodoCounter() {
+    const { totalTodos, completedTodos} = React.useContext(TodoContext)
+
     return (
         <>
             <section className="TodoHero">
                 <h1 className="TodoSalute"> Hey 👋</h1>
-                <h2 className="TodoCounter"> Has completado {completed} de {total} To Do's</h2>
+                <h2 className="TodoCounter"> Has completado {completedTodos} de {totalTodos} To Do's</h2>
             </section>
         </>
     );
